b03f46f9bc5d968892460b3dcbe598114eb90246
DIOGO FERREIRA DA CUNHA <diogo_f_cunha@estudante.sesisenai.org.br>
2025-09-19 08:54:41 -0300

    database connection

---
+++ src/db.js
@@ -0,0 +1,13 @@
+import mysql from "mysql2/promise";
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "diogo",
+    password: "root",
+    database: "sa_db",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+});
+
+export default db;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import db from './db.js';
 
 const app = express();
+app.use(express.json());
+
 const port = 3333;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+app.get('/usuarios', async (req, res) => {
+    try {
+        const [rows] = await db.query("SELECT * FROM usuarios");
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Erro no servidor");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Backend running in localhost:${port}`);
