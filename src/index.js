30ec925eb6360c25a7f318ffb9848c5e9f56d5e5
DIOGO FERREIRA DA CUNHA <diogo_f_cunha@estudante.sesisenai.org.br>
2025-09-19 08:22:50 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3333;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
+    console.log(`Backend running in localhost:${port}`);
+})
+
+export default app;
