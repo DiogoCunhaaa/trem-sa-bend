f3275fdcce34be04d5dddf3fd4997a239854f44f
DIOGO FERREIRA DA CUNHA <diogo_f_cunha@estudante.sesisenai.org.br>
2025-11-04 12:09:17 -0300

    variavel com a ultima msg

--- src/utils/mqtt/subscriber.js
+++ src/utils/mqtt/subscriber.js
@@ -2,10 +2,10 @@ import mqtt from "mqtt";
 
 const brokerURL = "mqtts://d8ab813c18e242c0a418f3b1609e5f94.s1.eu.hivemq.cloud";
 const options = {
-    username: "hivemq.webclient.1761663875221",
-    password: "Gih;Q3.H<0N9vm7oTrE,",
-    reconnectPeriod: 1000,
-}
+  username: "hivemq.webclient.1761663875221",
+  password: "Gih;Q3.H<0N9vm7oTrE,",
+  reconnectPeriod: 1000,
+};
 const client = mqtt.connect(brokerURL, options);
 
 const topic = "develop/S3";
@@ -22,10 +22,20 @@ client.on("connect", () => {
   });
 });
 
+export let ultimaMensagem = null; // importar em outro arquivo para usar;
 client.on("message", (topic, message) => {
   console.log(`Mensagem recebida em ${topic}: ${message.toString()}`);
+  ultimaMensagem = message.toString();
 });
 
 client.on("error", (err) => {
   console.error("Erro na conexão MQTT: ", err);
+});
+
+client.on("reconnect", () => {
+  console.log("Tentando reconectar ao broker...");
+});
+
+client.on("close", () => {
+  console.log("Conexão MQTT encerrada.");
 });
